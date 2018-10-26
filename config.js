fdff5de897e9652a0f30397942ecf47a58376c20
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-26 10:59:24 +0530

    Updated Boilerplate Minified Code.

--- config.js
+++ config.js
@@ -1,56 +1,108 @@
-module.exports = {
-	project_name: "VSP JS Helper",
-	scss: false,
-	js: {
-		"vsp-js-helper.js": {
-			dist: "./",
-			webpack: true,
-			babel: false,
-			watch: [ './parts/*.js', './parts/*' ],
-			combine_files: false,
-			concat: "vsp-js-helper.dist.js",
-		},
-	},
-	/**
-	 * Settings any feature to false will not trigger for any files untill its
-	 * overridden in file config.
-	 * js:{
-	 *     "your_file_source":{
-	 *         scss:true,
-	 *         dist:"your_file_dist",
-	 *     }
-	 * }
-	 */
-	status: {
-		combine_files: true,
-		minify: true,
-		concat: true,
-		scss: true,
-		sourcemap: true,
-		autoprefixer: true,
+let $_json               = {};
+$_json[ 'project_name' ] = 'VSP JS Helper';
+$_json[ 'scss' ]         = false;
+$_json[ 'js' ]           = {
+	"vsp-js-helper.js": {
+		dist: "./",
 		webpack: true,
 		babel: false,
-		parcel: false,
-		uglify: true,
-		rollup: false,
-	},
-
-	default_config: {
-		combine_files: { append: 'gulp-append', prepend: 'gulp-prepend', inline: 'gulp-inline', },
-		minify_css: { args: {}, callback: false },
-		concat: {},
-		scss: { outputStyle: "expanded" },
-		sourcemap: "../maps",
-		autoprefixer: {
-			browsers: [ "last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4" ],
-			cascade: false
-		},
-		webpack: {
-			mode: "production",
-			"target": "node",
-			output: { filename: '[name].js', },
-			module: { rules: [ { test: /\.js$/, loader: 'babel-loader', options: { presets: [ 'es2015' ] } } ] },
-		},
-		parcel: false,
+		watch: [ './parts/*.js', './parts/*', './index.js' ],
+		combine_files: false,
+		concat: "vsp-js-helper.dist.js",
 	},
 };
+
+/**
+ * Settings any feature to false will not trigger for any files untill its
+ * overridden in file config.
+ * js:{
+ *     "your_file_source":{
+ *         scss:true,
+ *         dist:"your_file_dist",
+ *     }
+ * }
+ */
+$_json[ 'status' ] = {
+	scss: true,
+	autoprefixer: true,
+	sourcemap: true,
+	webpack: true,
+	babel: false,
+	parcel: false,
+	rollup: false,
+	minify: true,
+	uglify: true,
+	combine_files: true,
+	concat: true,
+};
+$_json[ 'default_config' ] = {
+	/**
+	 * Production Configs.
+	 */
+	combine_files: {
+		append: 'wponion-append',
+		prepend: 'wponion-prepend',
+		inline: 'wponion-inline',
+	},
+	minify: {
+		args: {},
+		callback: false
+	},
+	concat: {},
+	scss: {
+		outputStyle: "expanded"
+	},
+	sourcemap: "../maps",
+	autoprefixer: {
+		browsers: [ "last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4" ],
+		cascade: false
+	},
+	webpack: {
+		mode: "production",
+		output: {
+			filename: '[name].js',
+		},
+		target: "node",
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					options: {
+						presets: [ 'es2015' ]
+					}
+				}
+			]
+		},
+	},
+	parcel: false,
+	uglify: true,
+	babel: {
+		presets: [ '@babel/env' ],
+	},
+
+	/**
+	 * Development Config.
+	 */
+	webpack_dev: {
+		devtool: "inline-source-map",
+		mode: "development",
+		target: "node",
+		output: {
+			filename: '[name].js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					options: {
+						presets: [ 'es2015' ]
+					}
+				}
+			]
+		},
+	},
+	uglify_dev: false,
+};
+module.exports             = $_json;
