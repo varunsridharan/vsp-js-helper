6b480a7cd0a61ec237241861033bba969f38097e
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-27 07:37:37 +0530

    minor update

--- parts/to_js_func.js
+++ parts/to_js_func.js
@@ -1,12 +1,10 @@
 import is_object_like from './is_object_like';
-import wrap_array from './wrap_array';
 import validateJSFunc from './validateSingleJSFunc';
 
-
-module.exports = ( $data ) => {
+module.exports = ( $data, $args_key = 'js_args', $contents_key = 'js_contents' ) => {
 	if( true === is_object_like( $data ) ) {
 		for( let $key in $data ) {
-			$data[ $key ] = validateJSFunc( $data[ $key ] );
+			$data[ $key ] = validateJSFunc( $data[ $key ], $args_key, $contents_key );
 		}
 	}
 	return $data;
