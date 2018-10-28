e2a0e5ee350ba201f36ec9f5523b0c28d49982e5
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-28 11:07:47 +0530

    Minor bug fixed

--- parts/to_js_func.js
+++ parts/to_js_func.js
@@ -1,10 +1,13 @@
 import is_object_like from './is_object_like';
 import validateJSFunc from './validateSingleJSFunc';
+import empty from 'locutus/php/var/empty';
 
 module.exports = ( $data, $args_key = 'js_args', $contents_key = 'js_contents' ) => {
 	if( true === is_object_like( $data ) ) {
 		for( let $key in $data ) {
-			$data[ $key ] = validateJSFunc( $data[ $key ], $args_key, $contents_key );
+			if( !empty( $data[ $key ] ) ) {
+				$data[ $key ] = validateJSFunc( $data[ $key ], $args_key, $contents_key );
+			}
 		}
 	}
 	return $data;
