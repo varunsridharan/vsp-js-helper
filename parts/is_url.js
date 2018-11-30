50216f564558b1f92fb48068f0e8fbb963d6b901
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-30 19:25:30 +0530

    bug fixed

--- parts/is_url.js
+++ parts/is_url.js
@@ -1,4 +1,4 @@
-export default ( $url ) => {
+module.exports = ( $url ) => {
 	let pattern = new RegExp( '^(https?:\/\/)?' + // protocol
 		'((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|' + // domain name
 		'((\d{1,3}\.){3}\d{1,3}))' + // OR ip (v4) address
