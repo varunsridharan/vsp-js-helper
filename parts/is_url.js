dee562bdae40e90269e0ceef2efda855493b5a48
Varun Sridharan <varunsridharan23@gmail.com>
2018-12-01 06:43:14 +0530

    updated

--- parts/is_url.js
+++ parts/is_url.js
@@ -1,9 +1,9 @@
 module.exports = ( $url ) => {
-	let pattern = new RegExp( '^(https?:\/\/)?' + // protocol
-		'((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|' + // domain name
-		'((\d{1,3}\.){3}\d{1,3}))' + // OR ip (v4) address
-		'(\:\d+)?(\/[-a-z\d%_.~+]*)*' + // port and path
-		'(\?[;&a-z\d%_.~+=-]*)?' + // query string
-		'(\#[-a-z\d_]*)?$', 'i' ); // fragment locater
+	let pattern = new RegExp( '^(https?:\\/\\/)?' + // protocol
+		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+		'((\\d{1,3}\\.){3}\\d{1,3}))' + // ip (v4) address
+		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + //port
+		'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+		'(\\#[-a-z\\d_]*)?$', 'i' );
 	return ( pattern.test( $url ) );
 };
